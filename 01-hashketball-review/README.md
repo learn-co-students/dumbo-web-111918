69dc2b4d9d2346d58433f1eaa928f77c946a421e
github username <github email address>
2018-11-20 02:34:13 -0500

    adds lecture

---
+++ 01-hashketball-review/README.md
@@ -0,0 +1,15 @@
+# Hashketball Review
+
+## Objectives
+
+* Distinguish between data types in Ruby
+* Break large problems into smaller problems
+* Learn how to have quick feedback loops
+* Show how to look up documentation for data types in Ruby
+* Demonstrate the use of common Array methods
+  * `#each`
+  * `#map`
+  * `#select`
+  * `#find`
+* Differentiate array methods by their respective return values
+* Define the Single Responsibility Principle
---
+++ 01-hashketball-review/hashketball.rb
@@ -0,0 +1,176 @@
+require 'pry'
+
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {
+          name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        {
+          name: "Reggie Evans",
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        },
+        {
+          name: "Brook Lopez",
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        },
+        {
+          name: "Mason Plumlee",
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+        {
+          name: "Jason Terry",
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
+      ]
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: [
+        {
+          name: "Jeff Adrien",
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        },
+        {
+          name: "Bismak Biyombo",
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
+        },
+        {
+          name: "DeSagna Diop",
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        },
+        {
+          name: "Ben Gordon",
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        },
+        {
+          name: "Brendan Haywood",
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
+        }
+      ]
+    }
+  }
+end
+
+def num_points_scored(player_name)
+# given the argument of a specific players name,
+# return the points value from that players hash
+
+# getting to the points
+# - iterate through the game hash
+#   - looking through home and away keys, find each team's players
+  find_player(player_name)[:points]
+# - go through each level of this nested data to get to the points
+
+end
+
+def shoe_size(player_name)
+  # binding.pry
+  find_player(player_name)[:shoe]
+end
+
+def all_players
+  away = game_hash[:away][:players]
+  home = game_hash[:home][:players]
+  home + away
+end
+
+def find_player(player_name)
+  all_players.find do |player_hash|
+    player_hash[:name] == player_name
+  end
+end
+  # all_players.find { |player_hash| }
+
+  # game_hash.each do |location, team_info|
+  #    # team_info[:players] => array of player hashes
+  #   team_info[:players].each do |player_hash|
+  #     #   - loop through them to find the right player
+  #     if player_hash[:name] == player_name
+  #       #   - return the players points
+  #       return player_hash
+  #     end
+  #   end
+  # end
+# end
+
+# all_players
+# find_player("Brendan Haywood")
+# shoe_size("Brendan Haywood")
+
+# binding.pry
