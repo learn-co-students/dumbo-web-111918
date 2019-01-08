5c158ca9617e3287427ee8d9f4b7ae20c4e4cef2
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-01-08 15:35:14 -0500

    fsjhkgsdfgsdfhgfjg

---
+++ 22-scope-closures/README.md
@@ -0,0 +1,53 @@
+# Key questions
+* What is the difference between primitives and non-primitives when it comes to pass-by-reference vs. pass-by-value?
+* What is the difference between = and == and === (!= and !==)
+
+
+* What are the differences between global, functional, and block scope
+* What are the similarities and differences between var, let, and const
+* What is a closure? What does a closure have to do with callbacks and scope? 
+
+## Additional key questions
+* switch - why vs if/else
+* filter and/or map
+  * reduce
+
+# Scope
+* Global scope
+* Function scope and var
+  * reassigning
+* Block scope
+  * let
+    * re-declaring
+  * const
+    * re-assigning
+
+
+
+## global
+- Can access them ANYWHERE :(
+## var
+- function scope ("leaks" out of blocks)
+- re-declare in the same scope with no error :(
+## let
+- block scope
+- can reassign
+- get an error if you try to re-declare in the same scope :)
+## const
+- block scope
+- can NOT reassign
+- get an error if you try to re-declare in the same scope :)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 22-scope-closures/index.js
@@ -0,0 +1,147 @@
+
+/* global */
+// garbage = "fire"
+
+// function potato() {
+//   trash = "fire"
+// }
+
+// potato()
+
+// console.log(garbage)
+// console.log(trash)
+
+/* var */
+
+// var foo = "bar"
+
+// var foo = "yo"
+
+
+
+// function potato() {
+//   var foo = "blat"
+//   console.log(foo)
+// }
+
+
+// potato();
+
+// console.log(foo)
+
+
+
+const foo = "bar"
+// let foo = "bar"
+
+foo = "baz"
+
+
+
+if (true){
+  let banana =  "potato" 
+  console.log(banana)
+}
+
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
+
+console.log(i);
+
+// bsdfkjhsdf();
+
+// consoxle.log(banana)
+
+
+// biggest difference between 
+
+
+
+
+
+
+// var foo = "bar"
+
+// if (foo = "potato") {
+//   console.log("FRIEEEESSSS")
+// }
+
+// if (foo == true) {
+//   console.log("POTATO PANCAKES")
+// }
+
+// var five = false
+
+// if (0 == "0") {
+//   console.log("fiiiiiiive");
+// }
+
+// if (five === "false") {
+//   console.log("heeeeeyyyyyyyyy")
+// }
+
+
+// if (foo === true) {
+//   console.log("Vodka!")
+// }
+
+
+// What's the difference between == and truthy/falsy
+// In line 9 if you change true to potato without quotes will it log
+// What's the best practice when it comes to == and ===
+// js(===) === ruby(==) ???????
+
+
+
+//primitive
+//string, number, undefined, null, booleans, symbols?
+
+//non-primitive
+//objects (arrays, functions, { foo: "bar"} )
+
+
+// var numInAnObject = {
+//   number: 5
+// }
+
+// function increment(whatever) {
+//   whatever.number++
+// }
+
+// function addSomeEmojis(whatever) {
+//   whatever.emojis = ["🍩", "🍩", "🍩", "🍩"]
+// }
+
+
+// increment(numInAnObject)
+// increment(numInAnObject)
+// increment(numInAnObject)
+// increment(numInAnObject)
+// increment(numInAnObject)
+// increment(numInAnObject)
+// increment(numInAnObject)
+
+// addSomeEmojis(numInAnObject)
+
+// console.log(numInAnObject)
+
+// Why is it that whatever happens in the function 
+// stays in the function? Because of pass-by-value?
+
+// Go further into object mutation via passing by reference?
+
+
+
+// var num = 5
+
+// function increment() {
+//   num++
+// }
+
+// increment()
+
+// console.log(num)
+
+
+
