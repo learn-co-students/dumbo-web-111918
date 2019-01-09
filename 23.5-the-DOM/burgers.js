11137f3c85308b2c969e4c1783bda321010042a5
Eric Kim <hyeokjungkim@gmail.com>
2019-01-08 22:19:33 -0500

    adds lecture

---
+++ 23.5-the-DOM/burgers.js
@@ -0,0 +1,72 @@
+function burgers(){
+  return (
+    [
+      {
+        "id": 1,
+        "name": "Good Burger",
+        "category": "Relatable",
+        "imgURL": "https://www.lovelesscafe.com/uploads/recipeimages/BBQBaconBurger-web.jpg"
+      },
+      {
+        "id": 2,
+        "name": "Sal's Burger",
+        "category": "Relatable",
+        "imgURL": "http://cdn-image.myrecipes.com/sites/default/files/styles/medium_2x/public/umami-bomb-burgers-ck.jpg"
+      },
+      {
+        "id": 3,
+        "name": "Sloppy Beef",
+        "category": "Relatable",
+        "imgURL": "https://www.budgetbytes.com/wp-content/uploads/2011/06/Sloppy-Joes-Plus-sandwich.jpg"
+      },
+      {
+        "id": 4,
+        "name": "Glamburger",
+        "category": "Bougie",
+        "imgURL": "https://i.ytimg.com/vi/zrnHrX_NCbY/hqdefault.jpg"
+      },
+      {
+        "id": 5,
+        "name": "Burgér",
+        "category": "Bougie",
+        "imgURL": "https://assets3.thrillist.com/v1/image/937699/size/tmg-article_default_mobile.jpg"
+      },
+      {
+        "id": 6,
+        "name": "Le Magnifique",
+        "category": "Bougie",
+        "imgURL": "https://wswd-wordpress-production.s3.amazonaws.com/content/uploads/2017/01/Delmonicos.jpg"
+      },
+      {
+        "id": 7,
+        "name": "HamBougie",
+        "category": "Bougie",
+        "imgURL": "https://timedotcom.files.wordpress.com/2016/05/160528_gal_burger_blingburger.jpg"
+      },
+      {
+        "id": 8,
+        "name": "Green Eggs and Hamburger",
+        "category": "Relatable",
+        "imgURL": "https://stupiddope.com/wp-content/uploads/2016/05/mcdonalds-green-angry-birds-burger-01.jpg"
+      },
+      {
+        "id": 9,
+        "name": "Veggie Burger",
+        "category": "Relatable",
+        "imgURL": "https://cookieandkate.com/images/2013/05/sweet-potato-black-bean-veggie-burgers-recipe-0.jpg"
+      },
+      {
+        "id": 10,
+        "name": "Spinach",
+        "category": "Bougie",
+        "imgURL": "https://www.organicfacts.net/wp-content/uploads/spinach.jpg"
+      },
+      {
+        "id": 11,
+        "name": "Cow-abunga",
+        "category": "Relatable",
+        "imgURL": "https://www.checkers.com/wp-content/uploads/2017/03/BigBuford_Feature_1000x1000-600x600.jpg"
+      }
+    ]
+  )
+}
